0272efdc2c4c8de812001e71a1be04652e44af6d
Cassinity <58294111+Cassinity@users.noreply.github.com>
2020-11-17 14:03:53 +0100

    Add files via upload

---
+++ Week6/changeJane.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+
+with open(sys.argv[1], 'r') as my_file:
+    for line in my_file.readlines():
+        old_name = line.strip()
+        new_name = old_name.replace("jane", "jdoe")
+        subprocess.run(["mv", old_name, new_name])
